907a4ea1ae489d46cf186f4bb3e420cdbb74efe6
Maxim Bazhenov <stadibenker@gmail.com>
2024-09-11 12:22:05 +0100

    added Domain project

---
+++ CleanArchitecture.Domain/LeaveAllocation.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NubmerOfDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
