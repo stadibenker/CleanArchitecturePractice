70f1e2141aede118c74b67a3c965c68438f1e632
Maxim Bazhenov <stadibenker@gmail.com>
2024-09-16 14:35:54 +0100

    updates

---
+++ CleanArchitecture.Persistance/PersistenceServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Persistence
+{
+	public static class PersistenceServiceRegistration
+	{
+		public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+		{
+			return services;
+		}
+	}
+}
