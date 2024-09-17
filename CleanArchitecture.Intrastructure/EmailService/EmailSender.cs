1af7af40d96f0d1f94b4a5ffea1898e9fa70a646
Maxim Bazhenov <stadibenker@gmail.com>
2024-09-17 13:25:31 +0100

    added email service implementetaion

--- CleanArchitecture.Intrastructure/EmailService/EmailSender.cs
+++ CleanArchitecture.Intrastructure/EmailService/EmailSender.cs
@@ -1,13 +1,34 @@
 ﻿using ClearArchitecture.Application.Contracts.Email;
 using ClearArchitecture.Application.Models.Email;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
 
 namespace CleanArchitecture.Infrastructure.EmailService
 {
 	public class EmailSender : IEmailSender
 	{
-		public Task<bool> SendEmail(EmailMessage email)
+		private EmailSettings _emailSettings {get;}
+		
+		public EmailSender(IOptions<EmailSettings> emailSettings) {
+			_emailSettings = emailSettings.Value;
+		}
+		
+		public async Task<bool> SendEmail(EmailMessage email)
 		{
-			throw new NotImplementedException();
+			var client = new SendGridClient (_emailSettings.ApiKey);
+			var to = new EmailAddress(email.To);
+			var from = new EmailAddress
+			{
+				Email = _emailSettings.FromAddress,
+				Name = _emailSettings.FromName
+			};
+
+			var message = MailHelper.CreateSingleEmail(from, to, email.Subject, email.Body, email.Body);
+			var response = await client.SendEmailAsync(message);
+
+			return response.IsSuccessStatusCode;
 		}
 	}
 }
--- CleanArchitecture.Intrastructure/InfrastructureServicesRegistration.cs
+++ CleanArchitecture.Intrastructure/InfrastructureServicesRegistration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using CleanArchitecture.Infrastructure.EmailService;
+using ClearArchitecture.Application.Contracts.Email;
+using ClearArchitecture.Application.Models.Email;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArchitecture.Infrastructure
@@ -7,7 +10,10 @@ namespace CleanArchitecture.Infrastructure
 	{
 		public static IServiceCollection ConfigureInfrastructuionServices (this IServiceCollection services, IConfiguration configuration)
 		{
-
+			services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+			services.AddTransient<IEmailSender, EmailSender>();
+			
+			return services;
 		}
 	}
 }
